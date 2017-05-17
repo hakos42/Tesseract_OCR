16ada38ceaab24975e1360a090dbfd6a4e94ad35
hakim djelili <hakos42@MacBook-Air-de-hakim.local>
2017-05-17 07:57:32 +0200

    Initial Commit

---
+++ Tesseract/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tesseract
+//
+//  Created by hakim djelili on 17/05/2017.
+//  Copyright © 2017 hakim djelili. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
